f4d72ab109fb467e740bcef18a3e354b545dfa58
Kenneth Ceglia <KPZNet@outlook.com>
2021-10-30 12:11:24 -0700

    dvs

--- kimager.py
+++ kimager.py
@@ -13,10 +13,23 @@ import cv2
 import os
 import numpy as np
 
+try:
+  from google.colab import drive
+  drive.mount('/content/drive')
+  IN_COLAB = True
+except:
+  IN_COLAB = False
+
+def get_root_drive():
+    root_patht = "flowers/train"
+    root_pathv = "flowers/validate"
+    if IN_COLAB:
+        root_patht = '/content/drive/MyDrive/Colab Notebooks/flowers/train'
+        root_pathv = '/content/drive/MyDrive/Colab Notebooks/flowers/validate'
+    return root_patht, root_pathv
 
 
-
-epochs = 3
+epochs = 10
 labels = ['daisy', 'rose']
 img_size = 224
 
@@ -43,8 +56,9 @@ def get_data(data_dir):
 
 isgpu2()
 
-train = get_data('flowers/train')
-val = get_data('flowers/validate')
+root_patht, root_pathv = get_root_drive()
+train = get_data(root_patht)
+val = get_data(root_pathv)
 
 l = []
 for i in train:
@@ -172,71 +186,60 @@ predictions = classes_x.reshape(1,-1)[0]
 print(classification_report(y_val, predictions, target_names = ['INITIAL Daisy (Class 0)','Rose (Class 1)']))
 
 
+def transfer_learner():
+    global model, learning_rate, opti, history, predict_x, classes_x, predictions, acc, val_acc, loss, val_loss, epochs_range
+    base_model = keras.applications.mobilenet_v2.MobileNetV2(input_shape=(224, 224, 3), include_top=False,
+                                                             weights="imagenet")
+    base_model.trainable = False
+    model = keras.Sequential([base_model,
+                              keras.layers.GlobalAveragePooling2D(),
+                              keras.layers.Dropout(0.2),
+                              keras.layers.Dense(2, activation="softmax")
+                              ])
+    learning_rate = 0.000001
+    opti = adam_v2.Adam(learning_rate=learning_rate, decay=learning_rate / epochs)
+    model.compile(optimizer=opti,
+                  loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                  metrics=['accuracy'])
+    history = model.fit(x_train, y_train, epochs=epochs, validation_data=(x_val, y_val))
+    predict_x = model.predict(x_val)
+    classes_x = np.argmax(predict_x, axis=1)
+    predictions = classes_x.reshape(1, -1)[0]
+    print(classification_report(y_val, predictions, target_names=['PRETRAINED Daisy (Class 0)', 'Rose (Class 1)']))
+    acc = history.history['accuracy']
+    val_acc = history.history['val_accuracy']
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+    epochs_range = range(epochs)
+    plt.figure(figsize=(15, 15))
+    plt.subplot(2, 2, 1)
+    plt.plot(epochs_range, acc, label='Training Accuracy')
+    plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+    plt.legend(loc='lower right')
+    plt.title('Training and Validation Accuracy')
+    plt.subplot(2, 2, 2)
+    plt.plot(epochs_range, loss, label='Training Loss')
+    plt.plot(epochs_range, val_loss, label='Validation Loss')
+    plt.legend(loc='upper right')
+    plt.title('Training and Validation Loss')
+    plt.show()
+    acc = history.history['accuracy']
+    val_acc = history.history['val_accuracy']
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+    epochs_range = range(epochs)
+    plt.figure(figsize=(15, 15))
+    plt.subplot(2, 2, 1)
+    plt.plot(epochs_range, acc, label='Training Accuracy')
+    plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+    plt.legend(loc='lower right')
+    plt.title('Training and Validation Accuracy')
+    plt.subplot(2, 2, 2)
+    plt.plot(epochs_range, loss, label='Training Loss')
+    plt.plot(epochs_range, val_loss, label='Validation Loss')
+    plt.legend(loc='upper right')
+    plt.title('Training and Validation Loss')
+    plt.show()
 
-base_model = keras.applications.mobilenet_v2.MobileNetV2(input_shape = (224, 224, 3), include_top = False, weights = "imagenet")
 
-base_model.trainable = False
-
-model = keras.Sequential([base_model,
-                                 keras.layers.GlobalAveragePooling2D(),
-                                 keras.layers.Dropout(0.2),
-                                 keras.layers.Dense(2, activation="softmax")
-                                ])
-
-learning_rate = 0.000001
-opti = adam_v2.Adam(learning_rate=learning_rate, decay=learning_rate/epochs)
-model.compile(optimizer=opti,
-              loss= keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-              metrics=['accuracy'])
-
-history = model.fit(x_train,y_train,epochs = epochs , validation_data = (x_val, y_val))
-
-
-
-predict_x=model.predict(x_val) 
-classes_x=np.argmax(predict_x,axis=1)
-
-predictions = classes_x.reshape(1,-1)[0]
-print(classification_report(y_val, predictions, target_names = ['PRETRAINED Daisy (Class 0)','Rose (Class 1)']))
-
-
-
-acc = history.history['accuracy']
-val_acc = history.history['val_accuracy']
-loss = history.history['loss']
-val_loss = history.history['val_loss']
-epochs_range = range(epochs)
-
-plt.figure(figsize=(15, 15))
-plt.subplot(2, 2, 1)
-plt.plot(epochs_range, acc, label='Training Accuracy')
-plt.plot(epochs_range, val_acc, label='Validation Accuracy')
-plt.legend(loc='lower right')
-plt.title('Training and Validation Accuracy')
-
-plt.subplot(2, 2, 2)
-plt.plot(epochs_range, loss, label='Training Loss')
-plt.plot(epochs_range, val_loss, label='Validation Loss')
-plt.legend(loc='upper right')
-plt.title('Training and Validation Loss')
-plt.show()
-
-acc = history.history['accuracy']
-val_acc = history.history['val_accuracy']
-loss = history.history['loss']
-val_loss = history.history['val_loss']
-epochs_range = range(epochs)
-
-plt.figure(figsize=(15, 15))
-plt.subplot(2, 2, 1)
-plt.plot(epochs_range, acc, label='Training Accuracy')
-plt.plot(epochs_range, val_acc, label='Validation Accuracy')
-plt.legend(loc='lower right')
-plt.title('Training and Validation Accuracy')
-
-plt.subplot(2, 2, 2)
-plt.plot(epochs_range, loss, label='Training Loss')
-plt.plot(epochs_range, val_loss, label='Validation Loss')
-plt.legend(loc='upper right')
-plt.title('Training and Validation Loss')
-plt.show()
+#transfer_learner()
